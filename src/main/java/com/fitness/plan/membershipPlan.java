cf9096f28e24ecb38aaaf73b0a9b9920c0a2b029
IT25101141 <IT25101141@my.Sliit.lk>
2026-05-12 11:16:35 +0530

    updated membershipPlan java class

--- src/main/java/com/fitness/plan/membershipPlan.java
+++ src/main/java/com/fitness/plan/membershipPlan.java
@@ -1,4 +1,38 @@
 package com.fitness.plan;
 
 public class membershipPlan {
-}
+
+     private String planID;
+     private String planName;
+     private String planType;
+
+     public membershipPlan(String planID, String planName, String planType) {
+         this.planID = planID;
+         this.planName = planName;
+         this.planType = planType;
+     }
+
+     public String getPlanID() {
+         return planID;
+     }
+
+     public void setPlanID(String planID) {
+         this.planID = planID;
+     }
+
+     public String getPlanName() {
+         return planName;
+     }
+
+     public void setPlanName(String planName) {
+         this.planName = planName;
+     }
+
+     public String getPlanType() {
+         return planType;
+     }
+
+     public void setPlanType(String planType) {
+         this.planType = planType;
+     }
+ }
